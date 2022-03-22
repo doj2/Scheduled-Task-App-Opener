77cd746d67696e0aa9420e14129f45eb33007278
Johnny Do <55112984+doj2@users.noreply.github.com>
2022-03-22 15:48:05 -0400

    Add files via upload

---
+++ ScheduledTaskAppOpener.ps1
@@ -0,0 +1,8 @@
+﻿$action = New-ScheduledTaskAction -Execute 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+$trigger = New-ScheduledTaskTrigger -Once -At 3:40pm
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "DemoTask" -Description "Google Chrome opening automatically"
+
+<#removing the scheduled task
+Get-ScheduledTask -TaskName "DemoTask"
+Unregister-ScheduledTask -TaskName "Demotask" -Confirm:$false
+#>
